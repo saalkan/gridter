f8fd3872ee8592ac376e4e0c241ae54469ee2ce9
saalkan <s.asligulalkan@gmail.com>
2024-06-22 16:17:38 +0300

    Add new tweets (Intersection Observer API))
    Add text from bacon ipsum on reaching bottom of the page.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// source: https://stackoverflow.com/a/31978777
+let tweetcount = 0;
+
 async function fetch_tweet() {
   try {
     const response = await fetch(`https://baconipsum.com/api/?type=meat-and-filler&sentences=2&format=text`);
@@ -8,9 +11,10 @@ async function fetch_tweet() {
     const tweet = await response.text();
 
     const last_element = document.getElementById("scroll_for_more");
-    for (let i = 0; i < 3; i++) {
-    let new_tweet = `<div><p>${tweet}</p></div>`;
-    last_element.insertAdjacentHTML('beforebegin', new_tweet);
+    for (let i = 0; i < 5; i++) {
+      let new_tweet = `<div><p>${tweet}</p></div>`;
+      last_element.insertAdjacentHTML('beforebegin', new_tweet);
+      tweetcount++;
     }    
   }
   
@@ -18,3 +22,26 @@ async function fetch_tweet() {
     console.error(error);
   }
 }
+
+// source: https://dev.to/trex777/infinite-scrolling-using-intersection-observer-api-118l
+document.addEventListener("DOMContentLoaded", () => {
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1
+  };
+
+  function handleIntersect(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (tweetcount !== 150) {
+          fetch_tweet();
+        }
+      }
+    });
+  }
+
+  let loader = document.getElementById("scroll_for_more");
+  let observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(loader);
+});
